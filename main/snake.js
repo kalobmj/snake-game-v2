47210b2e70aac95aa6970b5c22277ca3a6dd16c0
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-05-27 01:48:03 -0400

    css work

--- main/snake.js
+++ main/snake.js
@@ -1,6 +1,6 @@
 const title = document.getElementById('title');
 const canvas = document.getElementById('canvas');
-const border = document.getElementById('border');
+// const border = document.getElementById('border');
 const space = document.getElementById('space');
 const highScore = document.getElementById('high-score');
 const score = document.getElementById('score');
@@ -21,19 +21,6 @@ const cellSize = canvasHeight / 8;
 canvas.width = rows * cellSize;
 canvas.height = cols * cellSize;
 
-// set background border size to canvas size (so that it lays underneath)
-// -> you can have a canvas , but don't need to draw the cells out, since the image has them
-border.width = canvas.width;
-border.height = canvas.height;
-
-
-console.log(border.width);
-console.log(border.height);
-
-console.log(canvas.width);
-console.log(canvas.height);
-
-
 // set space background maxHeight to the height of canvas
 space.style.maxHeight = `${canvasHeight}px`;
 
@@ -48,11 +35,6 @@ space.style.maxHeight = `${canvasHeight}px`;
 // score will be points based on jewel
 // seperate score will also be just jewels eaten
 
-// border.addEventListener('load', () => {
-//     c.drawImage(border, 0, 0, canvas.width, canvas.height);
-
-// })
-
 // create game board on space image load
 space.addEventListener('load', () => {
     // creating copy of space img
@@ -62,19 +44,17 @@ space.addEventListener('load', () => {
     newSpaceImage.height = 300;
     newSpaceImage.width = 300;
 
-
-
     c.drawImage(newSpaceImage, 0, 0, canvas.width, canvas.height);
 
     // nested loop to draw game grid ontop of canvas image
-    // for (let i=0; i<rows; i++) {
-    //     for (let j=0; j<cols; j++) {
-    //         const isEven = (i + j) % 2 === 0;
-    //         let color = isEven ? '#ffffff18' : '#eeff0048';
+    for (let i=0; i<rows; i++) {
+        for (let j=0; j<cols; j++) {
+            const isEven = (i + j) % 2 === 0;
+            let color = isEven ? '#ffffff00' : '#00000040';
             
-    //         // fill in grids
-    //         c.fillStyle = color;
-    //         c.fillRect(cellSize * i, cellSize * j, cellSize, cellSize);
-    //     }
-    // };
+            // fill in grids
+            c.fillStyle = color;
+            c.fillRect(cellSize * i, cellSize * j, cellSize, cellSize);
+        }
+    };
 });

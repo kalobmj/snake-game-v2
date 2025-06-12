3531d7c7141e702aad7f8953bb74d5628cd675a7
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-06-11 22:24:33 -0400

    move

--- main/snake3.js
+++ main/snake3.js
@@ -22,19 +22,43 @@ function grid() {
 
 grid();
 
-let snake = [{x: 200, y: 200}];
+let snake = [{x: 200, y: 200}, {x: 250, y: 200}];
 let apple = [{x: 100, y: 100}];
 
 c.fillStyle = 'green';
 c.fillRect(snake[0].x, snake[0].y, cellSize, cellSize);
+c.fillRect(snake[1].x, snake[1].y, cellSize, cellSize);
+
+c.fillStyle = 'red';
+c.fillRect(apple[0].x, apple[0].y, cellSize, cellSize);
 
 function placeApple() {
+    let x1, y1, collision;
+
+    do {
+        collision = false;
+
+        x1 = Math.floor(Math.random() * cols) * cellSize;
+        y1 = Math.floor(Math.random() * rows) * cellSize;
+
+        for (let i = 0; i < snake.length; i++) {
+            let snakeX = snake[i].x;
+            let snakeY = snake[i].y;
+            if (checkCollision(x1, y1, snakeX, snakeY)) {
+                console.log('there was a collision');
+                collision = true;
+                console.log({ collision });
+                break;
+            }
+        }
+    } while (collision);
+
     c.fillStyle = 'red';
+    apple[0].x = x1;
+    apple[0].y = y1;
     c.fillRect(apple[0].x, apple[0].y, cellSize, cellSize);
+
 };
-
-placeApple();
-
 
 let interval;
 let direction = 'right';
@@ -47,9 +71,7 @@ function checkCollision(x1, y1, x2, y2) {
 }
 
 function move() {
-
-    let head = snake[0];
-    console.log({head});
+    let head = { ...snake[0] };
     
     if (direction === 'right') {
         head.x += cellSize; 
@@ -61,15 +83,37 @@ function move() {
         head.y += cellSize;
     }
 
+    let appleFound = false;
+
     if (checkCollision(head.x, head.y, apple[0].x, apple[0].y)) {
+        snake.unshift(head);
+        appleFound = true;
+    } else {
+        snake.unshift(head);
+        snake.pop();
+    };
 
+    grid();
 
-
+    if (appleFound) {
+        placeApple();
+    } else {
+        c.fillStyle = 'red';
+        c.fillRect(apple[0].x, apple[0].y, cellSize, cellSize);
     }
- 
 
-
+    for (let i = 0; i < snake.length; i++) {
+        c.fillStyle = 'green';
+        c.fillRect(snake[i].x, snake[i].y, cellSize, cellSize);
+    };
+    
+    console.log('move done');
 };
+
+// check collisons
+// check bounds
+
+// all inside of move function
 
 canvas.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowRight') {
@@ -89,6 +133,6 @@ canvas.addEventListener('keydown', (e) => {
 
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
-        move();
+        interval = setInterval(move, 500);
     }
-})
+});

3c8a30f231fd0c15094c75d576c47ddb268945c8
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-06-16 01:56:39 -0400

    function work

--- main/snake3.js
+++ main/snake3.js
@@ -1,3 +1,4 @@
+const playButton = document.getElementById('play-btn');
 const canvas = document.getElementById('canvas-1');
 const c = canvas.getContext('2d');
 
@@ -8,12 +9,12 @@ console.log({ cellSize });
 
 canvas.height = cellSize * rows;
 canvas.width = cellSize * cols;
-canvas.setAttribute('tabindex', 1);
-canvas.focus();
 
 let interval;
+let isGameRunning;
 let direction = 'right';
-let gameRunning = false;
+let apple = [];
+let snake = [];
 
 // 
 
@@ -37,24 +38,105 @@ function grid() {
 };
 
 function fillBoard() {
+    apple.push({ x: cellSize * 6, y: cellSize * 6 });
+    c.fillStyle = 'red';
+    c.fillRect(apple[0].x, apple[0].y, cellSize, cellSize);
 
+    snake.push({ x: cellSize * 2, y: cellSize * 4 });
+    snake.push({ x: cellSize * 3, y: cellSize * 4 });
+    c.fillStyle = 'green';
+    c.fillRect(snake[0].x, snake[0].y, cellSize, cellSize);
+    c.fillRect(snake[1].x, snake[1].y, cellSize, cellSize);
 };
 
 function placeApple() {
+    let x1, y1, collision;
 
+    do {
+        collision = true;
+
+        x1 = Math.floor(Math.random() * rows) * cellSize;
+        y1 = Math.floor(Math.random() * cols) * cellSize;
+
+        for (const cell of snake) {
+            if (!checkCollision(snake[0].x, snake[0].y, x1, y1)) {
+                collision = false;
+                break;
+            }
+        }
+    } while (collision);
+
+    apple[0].x = x1;
+    apple[0].y = y1;
+    c.fillStyle = 'red';
+    c.fillRect(apple[0].x, apple[0].y, cellSize, cellSize);
 };
 
 function checkBounds(x, y) {
-
+    if (x < 0 || y < 0 || x > cellSize * rows || y > cellSize * cols) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 function move() {
 
+    let head = { ...snake[0] };
+    console.log({ head });
+
+    if (checkBounds(head.x, head.y)) {
+        clearInterval(interval);
+        return;
+    };
+
+    if (direction === 'right') {
+        head.x += cellSize;
+    } else if (direction === 'left') {
+        head.x -= cellSize;
+    } else if (direction === 'up') {
+        head.y -= cellSize;
+    } else if (direction === 'down') {
+        head.y += cellSize;
+    };
+
+    snake.unshift(head);
+
+    if (checkCollision(head.x, head.y, apple[0].x, apple[0].y)) {
+        grid();
+        placeApple();
+
+        for (let i = 0; i < snake.length; i++) {
+            c.fillStyle = 'green';
+            c.fillRect(snake[i].x, snake[i].y, cellSize, cellSize);
+        };
+    } else {
+        for (let i = 1; i < snake.length; i++) {
+            if (checkCollision(snake[0].x, snake[0].y, snake[i].x, snake[i].y)) {
+                console.log('body collision found');
+                clearInterval(interval);
+                return;
+            }
+        };
+
+        snake.pop();
+        grid();
+
+        c.fillStyle = 'red';
+        c.fillRect(apple[0].x, apple[0].y, cellSize, cellSize);
+
+        for (let i = 0; i < snake.length; i++) {
+            c.fillStyle = 'green';
+            c.fillRect(snake[i].x, snake[i].y, cellSize, cellSize);
+        }
+    };
 };
 
 //
 
 grid();
+
+fillBoard();
 
 //
 
@@ -74,14 +156,14 @@ canvas.addEventListener('keydown', (e) => {
     }
 });
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        if (!gameRunning) {
-            gameRunning = true;
-            interval = setInterval(move, 500);
-        } else {
-            gameRunning = false;
-            clearInterval(interval);
-        }
+playButton.addEventListener('click', () => {
+    canvas.setAttribute('tabindex', 1);
+    canvas.focus();
+
+    if (!isGameRunning) {
+        interval = setInterval(move, 500);
+    } else {
+        clearInterval(interval);
+        isGameRunning = false;
     }
 });
